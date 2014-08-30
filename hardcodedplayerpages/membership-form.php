a7512c78cfffe92dff05981ffc798d64111037c0
Ben Wainwright <bwainwright28@gmail.com>
2014-08-30 19:41:28 +0100

    Removed extraneous whitespace

--- hardcodedplayerpages/membership-form.php
+++ hardcodedplayerpages/membership-form.php
@@ -1,8 +1,5 @@
 <?php
     wp_enqueue_script('dynamicforms');
-    
-    
-   
 
 $form_user = ( isset ( $_GET['player_id'] ) && current_user_can ('committee_perms') ) 
                 ? $_GET['player_id'] : get_current_user_id();
