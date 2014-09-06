2aa39df9c7f56142c895db77b79cfbde615463da
Ben Wainwright <bwainwright28@gmail.com>
2014-09-06 04:41:11 +0100

    Bugfix
    Modified post submission so that wp_update_post includes ID

--- submission_handling/post.php
+++ submission_handling/post.php
@@ -4,5 +4,6 @@ update_post_meta($post, 'event_id', $_POST['event_link'] );
 
 if ( $_POST['attr_user'] != $_POST['current_user'] )
 {
-    wp_update_post( array ( 'post_author' => $_POST['attr_user'] ) );
+    
+    wp_update_post( array ( 'ID' => $post, 'post_author' => $_POST['attr_user'] ) );
 }
