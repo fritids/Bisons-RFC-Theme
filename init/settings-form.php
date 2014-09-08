9c2cd6f524cf07feffffe1414bd4597152e22c73
Ben Wainwright <bwainwright28@gmail.com>
2014-09-08 11:56:54 +0100

    Refactoring
    Removing unused files

--- init/settings-form.php
+++
@@ -1,8 +0,0 @@
-<h2>Bisons Online Settings</h2>
-<form method="POST" action="options.php">
-    <?php 
-    settings_fields ( 'bisons-settings' );
-    do_settings_sections( 'bisons-settings' );
-    submit_button( );
-    ?>
-</form>
